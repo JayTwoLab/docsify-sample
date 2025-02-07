e9aa60ed7bc51a793bc2f048e15dda312e01eb86
Jay Two <j2doll@gmail.com>
2025-02-07 13:41:54 +0900

    Initial commit

---
+++ test/docs/_sidebar.md
@@ -0,0 +1,11 @@
+<!-- docs/_sidebar.md -->
+
+* [Home](/)
+* [hello](hello.md)
+
+---
+
+* [/test 한글 漢字](test/README.md)
+* [/test/more](test/more.md)
+
+
---
+++ test/start.cmd
@@ -0,0 +1,13 @@
+@rem
+@rem init  : docsify init ./docs
+@rem start : docsify serve docs
+@rem
+
+docsify serve docs
+
+
+@rem 파이썬으로 구동하는 방법 (node 필요 없음) 
+@rem  cd test
+@rem  python -m http.server 4000   (python 3 인 경우)
+
+
