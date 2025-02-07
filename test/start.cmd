6b8375691ca516ee842cb6e44acfb2a866d4add0
Jay Two <j2doll@gmail.com>
2025-02-08 01:32:37 +0900

    Update start.cmd

--- test/start.cmd
+++ test/start.cmd
@@ -2,12 +2,12 @@
 @rem init  : docsify init ./docs
 @rem start : docsify serve docs
 @rem
-
-docsify serve docs
-
+@rem docsify serve docs
 
 @rem 파이썬으로 구동하는 방법 (node 필요 없음) 
 @rem  cd test
-@rem  python -m http.server 4000   (python 3 인 경우)
+@rem  python -m http.server 4000   (python 3 인 경우) http://localhost:4000 으로 접속 가능
 
+cd test
+python -m http.server 4000
 
